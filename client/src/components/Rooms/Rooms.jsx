2dede6429609da9c1113c00c6bdedf03aba6555b
slaman-farcy <salmanfarcy253@gmail.com>
2024-09-17 18:05:06 +0600

    Implemented data fetching system with tanstack query

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -1,53 +1,42 @@
-import { useEffect, useState } from 'react'
-import Card from './Card'
-import Container from '../Shared/Container'
-import { useSearchParams } from 'react-router-dom'
-import Heading from '../Shared/Heading'
-import Loader from '../Shared/Loader'
+import Card from "./Card";
+import Container from "../Shared/Container";
+import Heading from "../Shared/Heading";
+import Loader from "../Shared/Loader";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const Rooms = () => {
-  const [rooms, setRooms] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [params, setParams] = useSearchParams()
-  const category = params.get('category')
-  
+  const axiosSecure = useAxiosSecure()
 
-  useEffect(() => {
-    setLoading(true)
-    fetch('./rooms.json')
-      .then(res => res.json())
-      .then(data => {
-        if (category) {
-          const filtered = data.filter(room => room.category === category)
-          setRooms(filtered)
-        } else setRooms(data)
-
-        setLoading(false)
-      })
-  }, [category])
+  const { data: rooms=[], isLoading} = useQuery({
+    queryKey: ['rooms'],
+    queryFn: async ( )=> {
+      const {data} = await axiosSecure.get('/rooms')
+      return data;
+    }
+  })
 
 
-
-  if (loading) return <Loader />
+  if (isLoading) return <Loader />;
   return (
     <Container>
       {rooms && rooms.length > 0 ? (
-        <div className='pt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          {rooms.map(room => (
+        <div className="pt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {rooms.map((room) => (
             <Card key={room._id} room={room} />
           ))}
         </div>
       ) : (
-        <div className=' flex items-center justify-center min-h-[calc(100vh-300px)]'>
+        <div className=" flex items-center justify-center min-h-[calc(100vh-300px)]">
           <Heading
             center={true}
-            title='No Rooms Available In This Category!'
-            subtitle='Please Select Other Categories.'
+            title="No Rooms Available In This Category!"
+            subtitle="Please Select Other Categories."
           />
         </div>
       )}
     </Container>
-  )
-}
+  );
+};
 
-export default Rooms
+export default Rooms;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,22 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import AuthProvider from './providers/AuthProvider'
-import { router } from './routes/Routes'
-import { Toaster } from 'react-hot-toast'
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
-import { HelmetProvider } from 'react-helmet-async'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
+import { router } from "./routes/Routes";
+import { Toaster } from "react-hot-toast";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <AuthProvider>
-      <Toaster />
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </HelmetProvider>
-)
+);
