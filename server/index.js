7cea32f9f3472a4da4497098dc662e3a71e4a545
slaman-farcy <salmanfarcy253@gmail.com>
2024-10-10 13:26:40 +0600

    user role update route implement

--- server/index.js
+++ server/index.js
@@ -121,6 +121,20 @@ async function run() {
       res.send(result);
     });
 
+    // Update a User role
+    app.patch("/users/update/:email", async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email }
+      const updateDoc = {
+        $set: {
+          ...user, timestamp: Date.now(),
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
     // Get all Rooms
     app.get("/rooms", async (req, res) => {
       const category = req.query.category;
